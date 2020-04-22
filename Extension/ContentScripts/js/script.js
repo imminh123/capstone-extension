2d51309a72a91c3aeb8a7fa0f4641ad891e494d5
Minh Nguyen <minhco12@gmail.comm>
2020-04-22 17:27:19 +0700

    fix ui

--- Extension/ContentScripts/js/script.js
+++ Extension/ContentScripts/js/script.js
@@ -6,90 +6,149 @@ var getCourseNameByURL = "";
 var getTeacherByURL;
 var getFolderByStudentId;
 $(document).ready(function () {
-    var info = '<div class="noteitContainer" id="noteitContainer">\
-    <input type="text" id ="newFolder" class="newFolder" />\
-    <input type="submit" id ="addNewFolder" class="addNewFolder" value="Add folder"/>\
-    <select id="selectHighlightFolder">\
-    <option value="">Others</option> <br>\
-        </select>\
-   <div class="arrow">\
-   </div>\
-   <div class="hightLight">\
-       <div id="yellow" name="#FFF83E" class="color"></div>\
-       <div id="green" name="#91EFA4" class="color"></div>\
-       <div id="blue" name="#48C6FF" class="color"></div>\
-       <div id="red" name="#FF7DB9" class="color"></div>\
-       <div id="orange" name="#FFC143" class="color"></div>\
-   </div>\
-   <input type="text" id="hiddenText" hidden>\
-   <div class="note section" >\
-       <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"\
-           x="0px" y="0px" width="357px" height="357px" viewBox="0 0 357 357"\
-           style="enable-background:new 0 0 357 357;" xml:space="preserve">\
-           <g>\
-               <g id="add">\
-                   <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />\
-               </g>\
-       </svg>\
-       <span class="addToNotes">Add to notes</span>\
-   </div>\
-   <div class="ask section">\
-       <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"\
-           x="0px" y="0px" width="535.5px" height="535.5px" viewBox="0 0 535.5 535.5"\
-           style="enable-background:new 0 0 535.5 535.5;" xml:space="preserve">\
-           <g>\
-               <g id="live-help">\
-                   <path\
-                       d="M446.25,0h-357c-28.05,0-51,22.95-51,51v357c0,28.05,22.95,51,51,51h102l76.5,76.5l76.5-76.5h102c28.05,0,51-22.95,51-51\
-                   V51C497.25,22.95,474.3,0,446.25,0z M293.25,408h-51v-51h51V408z M346.8,211.65l-22.95,22.95c-20.399,17.85-30.6,33.15-30.6,71.4\
-                   h-51v-12.75c0-28.05,10.2-53.55,30.6-71.4l30.601-33.15c10.2-7.65,15.3-20.4,15.3-35.7c0-28.05-22.95-51-51-51s-51,22.95-51,51\
-                   h-51c0-56.1,45.9-102,102-102c56.1,0,102,45.9,102,102C369.75,175.95,359.55,196.35,346.8,211.65z" />\
-               </g>\
-           </g>\
-       </svg>\
-       <span class="askYourTutor">Ask your tutor</span>\
-   </div>\
-   <div class="noteDetail" id="noteDetail">\
-<p class="firstTitle" id="firstTitle"></p>\
-<div class="dropdown">\
-    <select class="selectFolder" id="selectFolder2">\
-        <option value="AL">Alabama</option>\
-    </select>\
-    <input class="chosen-value" type="text" value="" placeholder="Type to filter">\
-    <ul class="value-list" id="selectFolder">\
-        <li>Alabama</li>\
-        <li>Alaska</li>\
-    </ul>\
-</div>\
-<p class="secondTitle" id="secondTitle"></p>\
-<textarea id="descNotes"></textarea>\
-<div class="addBtn" id="addBtn">\
-    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"\
-        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="357px" height="357px"\
-        viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve">\
-        <g>\
-            <g id="add">\
-                <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />\
-            </g>\
-    </svg>\
-    <span>Add to notes</span>\
-</div>\
-<div class="addAskBtn" id="addAskBtn">\
-    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"\
-        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="357px" height="357px"\
-        viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve">\
-        <g>\
-            <g id="add">\
-                <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />\
-            </g>\
-    </svg>\
-    <span>Add to ask</span>\
-</div>\
-</div>\
-';
+    var info = `<div class="noteitContainer" id="noteitContainer">
+    <input type="text" id ="newFolder" class="newFolder" />
+    <input type="submit" id ="addNewFolder" class="addNewFolder" value="Add folder"/>
+    <select id="selectHighlightFolder">
+    <option value="">Others</option> <br>
+        </select>
+   <div class="arrow">
+   </div>
+   <div class="hightLight">
+       <div class="selection">
+            <div id="yellow" name="#FFF83E" class="color"></div>
+            <div id="green" name="#91EFA4" class="color"></div>
+            <div id="blue" name="#48C6FF" class="color"></div>
+            <div id="red" name="#FF7DB9" class="color"></div>
+            <div id="orange" name="#FFC143" class="color"></div>
+       </div>
+       
+
+       <div class="folders">
+            <p>Saved to "Others"</p>
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="306px" height="306px" viewBox="0 0 306 306" style="enable-background:new 0 0 306 306;" xml:space="preserve">
+            <g>
+                <g id="keyboard-arrow-up">
+                    <polygon points="35.7,247.35 153,130.05 270.3,247.35 306,211.65 153,58.65 0,211.65 		"/>
+                </g>
+            </g>
+            </svg>
+       </div>
+
+       <div class="folderOption">
+           <div class="topPart">
+               <span>Where to save your highlight?</span>
+                <input type="text" placeholder="Search or add new folder" />
+           </div>
+           
+           <ul>
+               <li>Final test</li>
+               <li>Salva Espresso</li>
+               <li>Raquel Silvia</li>
+           </ul>
+
+           <div id="createFolderBtn">
+                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="357px" height="357px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve">
+                    <g>
+                        <g id="add">
+                            <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z"/>
+                        </g>
+                    </g>
+                </svg>
+                Create folder "bleh"
+            </div>
+       </div>
+   </div>
+   <input type="text" id="hiddenText" hidden>
+   <div class="note section" >
+       <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+           x="0px" y="0px" width="357px" height="357px" viewBox="0 0 357 357"
+           style="enable-background:new 0 0 357 357;" xml:space="preserve">
+           <g>
+               <g id="add">
+                   <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />
+               </g>
+       </svg>
+       <span class="addToNotes">Add to notes</span>
+   </div>
+   <div class="ask section">
+       <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+           x="0px" y="0px" width="535.5px" height="535.5px" viewBox="0 0 535.5 535.5"
+           style="enable-background:new 0 0 535.5 535.5;" xml:space="preserve">
+           <g>
+               <g id="live-help">
+                   <path
+                       d="M446.25,0h-357c-28.05,0-51,22.95-51,51v357c0,28.05,22.95,51,51,51h102l76.5,76.5l76.5-76.5h102c28.05,0,51-22.95,51-51
+                   V51C497.25,22.95,474.3,0,446.25,0z M293.25,408h-51v-51h51V408z M346.8,211.65l-22.95,22.95c-20.399,17.85-30.6,33.15-30.6,71.4
+                   h-51v-12.75c0-28.05,10.2-53.55,30.6-71.4l30.601-33.15c10.2-7.65,15.3-20.4,15.3-35.7c0-28.05-22.95-51-51-51s-51,22.95-51,51
+                   h-51c0-56.1,45.9-102,102-102c56.1,0,102,45.9,102,102C369.75,175.95,359.55,196.35,346.8,211.65z" />
+               </g>
+           </g>
+       </svg>
+       <span class="askYourTutor">Ask your tutor</span>
+   </div>
+   <div class="noteDetail" id="noteDetail">
+        <p class="firstTitle" id="firstTitle"></p>
+        <div class="dropdown">
+            <select class="selectFolder" id="selectFolder2">
+                <option value="AL">Alabama</option>
+            </select>
+            <input class="chosen-value" type="text" value="" placeholder="Type to filter">
+            <ul class="value-list" id="selectFolder">
+                <li>Alabama</li>
+                <li>Alaska</li>
+            </ul>
+        </div>
+        <p class="secondTitle" id="secondTitle"></p>
+        <textarea id="descNotes"></textarea>
+        <div class="addBtn" id="addBtn">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="357px" height="357px"
+                viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve">
+                <g>
+                    <g id="add">
+                        <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />
+                    </g>
+            </svg>
+            <span>Add to notes</span>
+        </div>
+        <div class="addAskBtn" id="addAskBtn">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="357px" height="357px"
+                viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve">
+                <g>
+                    <g id="add">
+                        <path d="M357,204H204v153h-51V204H0v-51h153V0h51v153h153V204z" />
+                    </g>
+            </svg>
+            <span>Add to ask</span>
+        </div>
+</div>`;
 
     $('body').append(info);
     var info = "";
+
+
+    //close all popup
+    closeAllPopup = function() {
+        noteDetailSection.hide();
+        folderOption.hide();
+    }
+
+    //get DOM element
+    var folderOption = $('.folderOption');
+    var folderHighlightSection = $('.folders');
+    var noteDetailSection = $('#noteDetail');
+    var dropdownSection = $('.dropdown');
+    
+    //set event to DOM
+    folderHighlightSection.on("click", (e) => {
+        folderOption.show();
+
+    })
+
     $(document.body).on("mouseup", function (e) {
         if(!window.location.href.startsWith("http://noteitfu.herokuapp.com/")){
             myFunction(e);
@@ -205,7 +264,7 @@ $(document).ready(function () {
                         var indexOfString = value.index;
                         var domContent1 = document.body.innerHTML;
                         var indices = getIndicesOf(stringFind, domContent1);
-                        //document.body.innerHTML = document.body.innerHTML.substring(0, indices[indexOfString]) + "<span style='background-color:" + color + ";'>" + stringFind + "</span>" + document.body.innerHTML.substring(indices[[indexOfString]] + stringFind.length)
+                        document.body.innerHTML = document.body.innerHTML.substring(0, indices[indexOfString]) + "<span style='background-color:" + color + ";'>" + stringFind + "</span>" + document.body.innerHTML.substring(indices[[indexOfString]] + stringFind.length)
                     });
                 },
                 error: function (data) {
@@ -232,7 +291,7 @@ $(document).ready(function () {
             dataType: "json",
             data: newFolder,
             success: function (data) {
-                alert("add new folder success");
+                alert("Add new folder success");
                 var selection = '';
                 $.each(getFolderByStudentId, function (key, value) {
                     if (value.courseCode === "Other") {
@@ -312,9 +371,9 @@ $(document).ready(function () {
     $(document.body).on("click", ".addToNotes", function () {
         if (getFolderByURL !== "") {
             $('#dropdown-btn').hide();
-            $('#selectFolder').hide();
+            dropdownSection.hide();
         } else {
-            $('#selectFolder').show();
+            dropdownSection.show();
             $('#dropdown-btn').show();
         }
         $('#noteDetail').show();
@@ -338,15 +397,16 @@ $(document).ready(function () {
             $('.secondTitle').empty();
             document.getElementsByClassName('firstTitle')[0].append('Choose Teacher:');
             document.getElementsByClassName('secondTitle')[0].append('Question:');
-            if (typeof getTeacherByURL !== "undefined") {
-                $.each(getTeacherByURL, function (key, value) {
-                    selection += '<option value="' + value._id + '">' + value.name + '</option> <br>';
-                });
-            }
-            $('#selectFolder').show();
-            $('#dropdown-btn').show();
-            $('#selectFolder').empty();
-            $('#selectFolder').html(selection);
+            // if (typeof getTeacherByURL !== "undefined") {
+            //     $.each(getTeacherByURL, function (key, value) {
+            //         selection += '<option value="' + value._id + '">' + value.name + '</option> <br>';
+            //     });
+            // }
+            // $('#selectFolder').show();
+            // $('#dropdown-btn').show();
+            // $('#selectFolder').empty();
+            // $('#selectFolder').html(selection);
+            setTeacherDataToSelectBox('#selectFolder');
         }
         //$('.dropdown').append(dropdown);
     });
@@ -422,6 +482,7 @@ $(document).ready(function () {
 
 //SHOW EXTENSION
 function myFunction(e) {
+    // closeAllPopup();
     var x = window.getSelection().toString();
     if (x.trim() !== "" && !$('#noteitContainer').is(e.target) && $('#noteitContainer').has(e.target).length === 0) {
         var selection = window.getSelection();
@@ -479,6 +540,73 @@ function parseJwt(token) {
     return JSON.parse(jsonPayload);
 };
 
+//Initiate dropdown
+function initiateDropdown() {
+        //set data to dropdown
+        const inputField = document.querySelector('.chosen-value');
+        const dropdown = document.querySelector('.value-list');
+        const dropdownArray = [... document.querySelectorAll('.value-list-item')];
+        dropdown.classList.add('open');
+        let valueArray = [];
+        dropdownArray.forEach(item => {
+        valueArray.push(item.textContent);
+        });
+    
+        const closeDropdown = () => {
+        dropdown.classList.remove('open');
+        }
+    
+        inputField.addEventListener('input', () => {
+        dropdown.classList.add('open');
+        let inputValue = inputField.value.toLowerCase();
+        let valueSubstring;
+        if (inputValue.length > 0) {
+            for (let j = 0; j < valueArray.length; j++) {
+            if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
+                dropdownArray[j].classList.add('closed');
+            } else {
+                dropdownArray[j].classList.remove('closed');
+            }
+            }
+        } else {
+            for (let i = 0; i < dropdownArray.length; i++) {
+            dropdownArray[i].classList.remove('closed');
+            }
+        }
+        });
+    
+        dropdownArray.forEach(item => {
+        item.addEventListener('click', (evt) => {
+            inputField.value = item.textContent;
+            inputField.setAttribute('data-id', item.dataset.value);
+            dropdownArray.forEach(dropdown => {
+            dropdown.classList.add('closed');
+            });
+        });
+        })
+    
+        inputField.addEventListener('focus', () => {
+        inputField.placeholder = 'Search';
+        dropdown.classList.add('open');
+        dropdownArray.forEach(dropdown => {
+            dropdown.classList.remove('closed');
+        });
+        });
+    
+        inputField.addEventListener('blur', () => {
+        inputField.placeholder = 'Choose folder';
+        dropdown.classList.remove('open');
+        });
+    
+        document.addEventListener('click', (evt) => {
+        const isDropdown = dropdown.contains(evt.target);
+        const isInput = inputField.contains(evt.target);
+        if (!isDropdown && !isInput) {
+            dropdown.classList.remove('open');
+        }
+        });
+}
+
 // FILL DATA TO SELECT BOX
 function setDataToSelectBox(folderName){
     var selection = '';
@@ -490,71 +618,23 @@ function setDataToSelectBox(folderName){
     });
     $(folderName).empty();
     $(folderName).html(selection);
+    initiateDropdown();
+};
 
-
-    //set data to dropdown
-    const inputField = document.querySelector('.chosen-value');
-    const dropdown = document.querySelector('.value-list');
-    const dropdownArray = [... document.querySelectorAll('.value-list-item')];
-    dropdown.classList.add('open');
-    let valueArray = [];
-    dropdownArray.forEach(item => {
-    valueArray.push(item.textContent);
-    });
-
-    const closeDropdown = () => {
-    dropdown.classList.remove('open');
-    }
-
-    inputField.addEventListener('input', () => {
-    dropdown.classList.add('open');
-    let inputValue = inputField.value.toLowerCase();
-    let valueSubstring;
-    if (inputValue.length > 0) {
-        for (let j = 0; j < valueArray.length; j++) {
-        if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
-            dropdownArray[j].classList.add('closed');
-        } else {
-            dropdownArray[j].classList.remove('closed');
-        }
-        }
-    } else {
-        for (let i = 0; i < dropdownArray.length; i++) {
-        dropdownArray[i].classList.remove('closed');
-        }
-    }
-    });
-
-    dropdownArray.forEach(item => {
-    item.addEventListener('click', (evt) => {
-        inputField.value = item.textContent;
-        inputField.setAttribute('data-id', item.dataset.value);
-        dropdownArray.forEach(dropdown => {
-        dropdown.classList.add('closed');
+function setTeacherDataToSelectBox(folderName){
+    var dropdownSection = $('.dropdown');
+    var selection = '';
+    if (typeof getTeacherByURL !== "undefined") {
+        $.each(getTeacherByURL, function (key, value) {
+            // selection += '<option value="' + value._id + '">' + value.name + '</option> <br>';
+            selection += `<li class="value-list-item" data-value="${value._id}">${value.name}</li>`;
         });
-    });
-    })
-
-    inputField.addEventListener('focus', () => {
-    inputField.placeholder = 'Search';
-    dropdown.classList.add('open');
-    dropdownArray.forEach(dropdown => {
-        dropdown.classList.remove('closed');
-    });
-    });
-
-    inputField.addEventListener('blur', () => {
-    inputField.placeholder = 'Choose folder';
-    dropdown.classList.remove('open');
-    });
-
-    document.addEventListener('click', (evt) => {
-    const isDropdown = dropdown.contains(evt.target);
-    const isInput = inputField.contains(evt.target);
-    if (!isDropdown && !isInput) {
-        dropdown.classList.remove('open');
     }
-    });
+    dropdownSection.show();
+    $(folderName).show();
+    $(folderName).empty();
+    $(folderName).html(selection);
+    initiateDropdown();
 };
 
 
